5914b3175f6a61c057e93b2357ea5bb985bb25ca
Kenneth Davidson <xylene2016@protonmail.com>
2025-01-11 10:54:22 -0500

    Update build-haproxy.sh

--- ub2204/build-haproxy.sh
+++ ub2204/build-haproxy.sh
@@ -343,7 +343,7 @@ _build_haproxy() {
     set -e
     _tmp_dir="$(mktemp -d)"
     cd "${_tmp_dir}"
-    _haproxy_ver="$(wget -qO- 'https://www.haproxy.org/' | grep -i 'src/haproxy-' | sed 's/"/\n/g' | grep '^/download/' | grep -i '\.gz$' | sed -e 's|.*haproxy-||g' -e 's|\.tar.*||g' | grep -ivE 'alpha|beta|rc[1-9]' | grep '^3\.0' | sort -V | tail -n 1)"
+    _haproxy_ver="$(wget -qO- 'https://www.haproxy.org/' | grep -i 'src/haproxy-' | sed 's/"/\n/g' | grep '^/download/' | grep -i '\.gz$' | sed -e 's|.*haproxy-||g' -e 's|\.tar.*||g' | grep -ivE 'alpha|beta|rc[1-9]' | grep '^3\.1' | sort -V | tail -n 1)"
     wget -c -t 9 -T 9 "https://www.haproxy.org/download/${_haproxy_ver%.*}/src/haproxy-${_haproxy_ver}.tar.gz"
     tar -xof haproxy-*.tar*
     sleep 1
@@ -372,6 +372,7 @@ _build_haproxy() {
     USE_SYSTEMD=1 \
     USE_GETADDRINFO=1 \
     USE_PROMEX=1 \
+    USE_QUIC=1 \
     ADDLIB="-lz -ldl -pthread" \
     LDFLAGS="${LDFLAGS}"
 
